e6529c941ee74a9fe22d7878471da88451aa656e
DaveOkpare <okparedave@gmail.com>
2023-04-07 21:30:53 +0100

    Added configurations to load csv and turn them to dataframes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 
+import pandas as pd
 from dotenv import load_dotenv
 from kaggle.api.kaggle_api_extended import KaggleApi
 from langchain.agents import initialize_agent, Tool
@@ -14,6 +15,7 @@ class Kaggle:
         self.kaggle = KaggleApi()
         self.kaggle.authenticate()
         self.path = os.getcwd()
+        # self.openai = os.getenv("OPEN_AI_KEY") if os.getenv("OPEN_AI_KEY") else input("Enter your OpenAI Keys")
         self.folder = None
 
     def search(self, query):
@@ -44,25 +46,16 @@ class Kaggle:
         except Exception as e:
             return str(e)
 
-
-"""
-This method is unused. But the idea is to dynamically create dataframes for extracted .csv files.
-
     @staticmethod
-    def filter(path):
-        try:
-            output = []
-            for file_path in os.listdir(path):
-                file_name = file_path.split('/')[-1]
-                split_file_name = file_name.split(".")
-                if split_file_name[-1] == "csv":
-                    _ = os.path.join(path, file_path)
-                    locals()["_".join([split_file_name[0], "df"])] = pd.read_csv(_)
-                    return f'pd.read_csv({locals()["_".join([split_file_name[0], "df"])]})'
-            return str(output)
-        except Exception as e:
-            return str(e)
-"""
+    def create(path):
+        for file_path in os.listdir(path):
+            file_name, ext = file_path.split("/")[-1].split(".")
+            if ext == "csv":
+                csv_file = os.path.join(path, file_path)
+                globals()["_".join([file_name.lower(), "df"])] = pd.read_csv(csv_file)
+        data_frames = [i for i in globals().keys() if i.endswith("df")]
+        return f"Here are the dataframes created: {data_frames}"
+
 
 # Load the tool configs that are needed.
 kaggle = Kaggle()
@@ -78,12 +71,18 @@ tools = [
         func=kaggle.extract,
         description="useful for when you need to extract downloaded files",
     ),
+    Tool(
+        name="Create",
+        func=kaggle.create,
+        description="useful for when you need to create dataframes from extracted files",
+    ),
 ]
 
 llm = OpenAI(temperature=0, openai_api_key=os.getenv("OPEN_AI_KEY"))
 
 agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
 
-
 if __name__ == "__main__":
-    agent.run("Download the 'heptapod/titanic' dataset and extract it")
+    agent.run(
+        "Download abecklas/fifa-world-cup and extract and create dataframes from it"
+    )
